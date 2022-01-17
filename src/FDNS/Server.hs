351e318f1eeba3760539779af4149959104fd0f7
lucasdc6 <lucasdicunzolo@gmail.com>
2022-01-17 00:35:13 -0300

    Se agrega biblioteca para logs y se emprolija el cóidigo

--- src/FDNS/Server.hs
+++ src/FDNS/Server.hs
@@ -1,51 +1,50 @@
 module FDNS.Server where
 
 import Data.Word                  (Word16, Word32)
+import Data.Maybe                 (fromMaybe)
 import Control.Monad              (forever)
 import Network.Socket             (getAddrInfo, socket, addrAddress, addrFamily, bind, defaultProtocol, SocketType(Datagram))
 import Data.ByteString as BS      (unpack, take, drop)
 import Network.Socket.ByteString  (recvFrom, sendAllTo)
+import Colog                      ((<&), logStringStdout)
 
 import FDNS.Types
+import FDNS.Config as C
 import FDNS.Parsers.Parsers
 
 runUDPServer :: String -> String -> IO ()
 runUDPServer host port = do
+  let logger = logStringStdout
   addrinfos <- getAddrInfo Nothing (Just host) (Just port)
   let serveraddr = Prelude.head addrinfos
   sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
   bind sock (addrAddress serveraddr)
-  print ("UDP server is waiting at " ++ host ++ ":" ++ port)
+  logger <& ("UDP server is waiting at " ++ host ++ ":" ++ port)
   forever $ do
     (rawMessage, sockAddr) <- recvFrom sock 4096
-    print "Header: "
-    print (BS.unpack (BS.take 12 rawMessage))
-    print (BS.take 12 rawMessage)
-    print "Raw message: "
-    print (BS.unpack (BS.drop 12 rawMessage))
-    print (BS.drop 12 rawMessage)
+    logger <& ("Header: " ++ show (BS.unpack (BS.take 12 rawMessage)))
+    logger <& ("Body: " ++ show (BS.take 12 rawMessage))
+    logger <& ("Raw message: " ++ show (BS.unpack (BS.drop 12 rawMessage)))
+    logger <& ("Raw body: " ++ show (BS.drop 12 rawMessage))
     let message = unpackMessage rawMessage
-    print "Message: "
-    print message
+    logger <& ("Message: " ++ show message)
+    let question' = head (question message)
+    let record = C.lookup (qname question') (qtype question')
     let answer = DNSResource {
       rname = ".google.com",
       rtype = A,
       rclass = IN,
       ttl = 300::Word32,
       rdlength = 4::Word16,
-      rdata = "172.17.0.2"
+      rdata = value (fromMaybe Domain{} record)
     }
-    let message' = appendAnswer message answer
-    print "Message': "
-    print message'
+    let message' = setQueryResponse (appendAnswer message answer)
+    logger <& ("Message': " ++ show message')
     let response = packMessage message'
-    print "Response: "
-    print response
-    print "Response body:"
-    print (BS.unpack (BS.drop 12 response))
-    print (BS.drop 12 response)
+    logger <& ("Response: " ++ show response)
+    logger <& ("Response body: " ++ show (BS.unpack (BS.drop 12 response)))
+    logger <& show (BS.drop 12 response)
     let testMessage = unpackMessage response
-    print "Response parsed: "
-    print testMessage
+    logger <& ("Response parsed: " ++ show testMessage)
     sendAllTo sock response sockAddr
 
